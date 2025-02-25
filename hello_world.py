a1424281a42b57e221564165c685b2f27b6ec105
Gopinadh110 <gpragada@albany.edu>
2025-02-25 18:33:01 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,4 @@
 print("Hello, world!")
 print("Hello This is Ashok Reddy Bhimavarapu. I am one of his teammate")
 print("Hello Everyone, I'm divya sri jampana")
-
-
+print("Hello Everyone, I'm gopi")
